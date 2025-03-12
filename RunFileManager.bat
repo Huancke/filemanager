ee1ce4d6c6813ed60a822687918bacf90da7cca9
Huancke <38266019+Huancke@users.noreply.github.com>
2025-03-13 01:50:31 +0800

    Auto commit on file change

--- RunFileManager.bat
+++ RunFileManager.bat
@@ -1,11 +1,11 @@
 @echo off
 echo 正在启动文件管理器...
-cd /d D:\Code\FileManager\bin\Debug\net8.0-windows
+cd /d D:\Code\FileManager\NewFileManager\FileManager\bin\Debug\net8.0-windows
 echo 当前目录: %CD%
 echo 检查文件是否存在...
 if exist FileManager.exe (
     echo 文件存在，正在启动...
-    powershell -Command "Start-Process 'FileManager.exe' -Verb RunAs"
+    start FileManager.exe
     echo 启动命令已执行
 ) else (
     echo 错误: FileManager.exe 不存在!
