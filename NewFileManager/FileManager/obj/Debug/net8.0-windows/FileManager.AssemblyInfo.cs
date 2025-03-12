f4751b167d3c0eeece5f0ef305705a2cae44163b
Huancke <38266019+Huancke@users.noreply.github.com>
2025-03-13 01:50:50 +0800

    Auto commit on file change

--- NewFileManager/FileManager/obj/Debug/net8.0-windows/FileManager.AssemblyInfo.cs
+++ NewFileManager/FileManager/obj/Debug/net8.0-windows/FileManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd6cd7b8ba7cacd717dab1f27d0588b198a2c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae45db6f7cea390b548892952808d5ff4652401c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
